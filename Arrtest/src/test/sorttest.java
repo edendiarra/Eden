8d810a845421d2805d8cd29e38b17fddd240cdc2
193 <193@DESKTOP-DLOI120>
2017-01-16 14:10:33 +0900

    se

---
+++ Arrtest/src/test/sorttest.java
@@ -0,0 +1,17 @@
+package test;
+
+public class sorttest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int[]nums={57,5,10,7,63,9};
+			sort(nums,1);
+			sort(nums,2);
+	}
+	private static void sort(int[]nums,int orderby)
+	{
+		//이부분을 직접구현
+	}
+
+}
